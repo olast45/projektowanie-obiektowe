387e211260f97948e141e53a7361c79db80cf286
olast45 <olastepien554@gmail.com>
2026-03-31 21:29:56 +0200

    bubble sort

--- zadanie1/numbers.pas
+++ zadanie1/numbers.pas
@@ -15,18 +15,38 @@ var
 begin
   randomize; 
   for i := 1 to 50 do
-  begin
-    Table[i] := random(101); 
-  end;
+    Table[i] := random(101);
+end;
+
+procedure SortNumbers(var Table: TNumbers);
+var
+  i, j, temp: Integer;
+begin
+  for i := 1 to 49 do
+    for j := 1 to 50 - i do
+      if Table[j] > Table[j + 1] then
+      begin
+        temp := Table[j];
+        Table[j] := Table[j + 1];
+        Table[j + 1] := temp;
+      end;
 end;
 
 begin
   clrscr;
   
-  writeln('Generating 50 numbers...');
+  writeln('Generating numbers...');
   GenerateNumbers(Numbers);
+
+  writeln('Before sorting:');
+  for i := 1 to 50 do
+    write(Numbers[i], ' ');
+    
+  writeln;
   
-  writeln('Numbers:');
+  SortNumbers(Numbers);
+
+  writeln('After sorting:');
   for i := 1 to 50 do
     write(Numbers[i], ' ');
     
