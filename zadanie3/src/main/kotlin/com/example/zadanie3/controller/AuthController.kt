38bf0f204436ce91f3678882450ff6b3aa8de8a2
olast45 <olastepien554@gmail.com>
2026-04-15 20:54:01 +0200

    inject the service via autowired

--- zadanie3/src/main/kotlin/com/example/zadanie3/controller/AuthController.kt
+++ zadanie3/src/main/kotlin/com/example/zadanie3/controller/AuthController.kt
@@ -3,13 +3,14 @@ package com.example.zadanie3.controller
 import com.example.zadanie3.dto.LoginRequest
 import com.example.zadanie3.service.AuthService
 import org.springframework.web.bind.annotation.*
+import org.springframework.beans.factory.annotation.Autowired
 
 @RestController
-class AuthController() {
+class AuthController @Autowired constructor(private val authService: AuthService) {
 
     @PostMapping("/login")
     fun login(@RequestBody request: LoginRequest): String {
-    if (!AuthService.instance.authenticate(request.username, request.password)) {
+    if (!authService.authenticate(request.username, request.password)) {
         return "Something went wrong!"
     }
         return "Success"
--- zadanie3/src/main/kotlin/com/example/zadanie3/service/AuthService.kt
+++ zadanie3/src/main/kotlin/com/example/zadanie3/service/AuthService.kt
@@ -2,11 +2,8 @@ package com.example.zadanie3.service
 
 import org.springframework.stereotype.Service
 
-class AuthService private constructor() {
-
-    companion object {
-        val instance = AuthService() 
-    }
+@Service
+class AuthService {
 
     fun authenticate(username: String, password: String): Boolean {
         return username.isNotBlank() && password.isNotBlank()
