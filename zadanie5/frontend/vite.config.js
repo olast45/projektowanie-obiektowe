0fed2fb78da20724f26626f0456910b0c6c70c03
olast45 <olastepien554@gmail.com>
2026-06-09 21:25:21 +0200

    fix allowed hosts

--- zadanie5/frontend/vite.config.js
+++ zadanie5/frontend/vite.config.js
@@ -5,8 +5,8 @@ import babel from '@rolldown/plugin-babel'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    allowedHosts: "ex5-frontend.onrender.com"
-  }, 
+    allowedHosts: ["ex5-frontend.onrender.com"]
+  },
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
